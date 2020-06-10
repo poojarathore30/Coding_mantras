65f3e27285ee186cfadb91e5bd06bcb8bd15402e
ana ryan <53404072+poojarathore30@users.noreply.github.com>
2020-06-10 14:39:44 +0530

    Add files via upload

---
+++ unknown no of input.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<map>
+using namespace std;
+int main(){
+   //if therre are unknown no of input
+   int val;
+   while(cin>>val){
+   cout<<val<<endl;
+   }
+	return 0;
+}
